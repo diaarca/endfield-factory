5969532156f8ecfd6b408ff520a7c89279700a65
Dylan Grousson <59664001+diaarca@users.noreply.github.com>
2026-05-26 15:28:45 +0200

    working but not very beautiful

--- flake.nix
+++ flake.nix
@@ -18,21 +18,67 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        build-dir = "build";
+        executable = "aic_planner";
+
+        configure = pkgs.writeShellScriptBin "configure" ''
+          cmake -B ${build-dir} -S . -DCMAKE_BUILD_TYPE=Release
+        '';
+
+        build = pkgs.writeShellScriptBin "build" ''
+          if [ ! -f ${build-dir}/CMakeCache.txt ]; then
+              echo "Configuration not found. Running configure first..."
+              configure
+          fi
+          cmake --build ${build-dir} --parallel
+        '';
+
+        clean = pkgs.writeShellScriptBin "clean" ''
+          rm -rf ${build-dir}
+        '';
+
+        run = pkgs.writeShellScriptBin "run" ''
+          if [ ! -f ${build-dir}/bin/${executable} ]; then
+              echo "Executable not found. Running build first..."
+              build
+          fi
+          ./${build-dir}/bin/${executable} "$@"
+        '';
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            # dependencies
+            cmake
+            pkg-config
+            clp
             or-tools
             gnumake
             clang
-            bear
             fast-cpp-csv-parser
+            zlib
+            bzip2
+            abseil-cpp
+            protobuf
+            re2
+            clp
+            cbc
+            glpk
+            highs
+            eigen
+            scip
+
+            # custom commands
+            configure
+            build
+            clean
+            run
           ];
 
           shellHook = ''
             export ORTOOLS_DIR=${pkgs.or-tools}
             export CSV_PARSER_DIR=${pkgs.fast-cpp-csv-parser}
-            echo "AIC Planner dev shell loaded with or-tools and fast-cpp-csv-parser"
           '';
         };
       }
